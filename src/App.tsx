ace490b6d7f4d2fe55f4fcff1d1193acf0f097d9
smazydev <alizafar1437@gmail.com>
2021-05-10 23:55:30 -0400

    added types for react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React, { FC } from "react";
+//Components Imports
 import LoginForm from "./components/LoginForm";
 import LoginCreds from "./components/LoginCreds";
+import QuizPage from "./components/QuizPage";
+//React Router Imports
+import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
+
 
 const App:FC = () => {
   return (
     <div style={{height:"100vh"}}>
       <LoginForm />
       <LoginCreds />
+      <QuizPage />
     </div>
   );
 };
